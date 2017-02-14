bd4553be9e5a44ead6a5045ccd732b7f972a9fb8
EugeneThomas <ethomas1@stuy.edu>
2017-02-14 10:56:31 -0500

    First step to success

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -17,19 +17,55 @@
 
   ANALYSIS:
   <INSERT YOUR RESULTS ANALYSIS HERE>
-  ======================================*/
+  ======================================*/ 
 
 public class MergeSortTester 
-{
 
+{
     /******************************
      * execution time analysis 
      * <INSERT YOUR DESCRIPTION HERE OF 
      *  YOUR APPARATUS FOR GENERATING EXECUTION 
      *  TIME DATA...>
      ******************************/
+
     public static void main( String[] args ) 
     {
+	int[] one = new int[1]; 
+        MergeSort.mess(one); 
+        long begin = System.currentTimeMillis(); 
+	MergeSort.sort(one); 
+        long end = System.currentTimeMillis();
+	System.out.println("Time for one: " + (end-begin));
+
+
+
+	int[] ten = new int[10];
+	MergeSort.mess(ten); 
+        begin = System.currentTimeMillis();
+	MergeSort.sort(ten); 
+        end = System.currentTimeMillis();
+	System.out.println("Time for ten: " + (end-begin)); 
+
+
+
+	int[] hun = new int[100]; 
+	MergeSort.mess(hun); 
+        begin = System.currentTimeMillis();
+	MergeSort.sort(hun); 
+        end = System.currentTimeMillis();
+	System.out.println("Time for hundred: " + (end-begin));
+
+
+
+	int[] thou = new int[1000]; 
+	MergeSort.mess(thou); 
+        begin = System.currentTimeMillis();
+	MergeSort.sort(thou); 
+        end = System.currentTimeMillis();
+	System.out.println("Time for thousand: " + (end-begin));
+
+
 
     }//end main
 
