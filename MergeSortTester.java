f2d3ee461a6cfc50a634a24c4040a8454cb94817
Annie <Annie>
2017-04-05 19:21:13 -0400

    optimized by making methods

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -14,6 +14,8 @@ HW7-- What Does the Data Say?
 
   BIG-OH CLASSIFICATION OF ALGORITHM: nlog(n)
 
+
+  *OLD TIMES================================
   Mean execution times for dataset of size n:
   Batch size: 4
   n=1            time: 0.50975 
@@ -27,6 +29,20 @@ HW7-- What Does the Data Say?
   n=1,000,000    time: 134361.51275
   n=10,000,000   time: 1149694.99275
   n=100,000,000  time: 1.29494E7
+  ===========================================
+
+  *NEW TIMES================================
+  Mean execution times for dataset of size n:
+  Batch size: 50
+  n=1            time: 0.0006
+  n=10           time: 0.0247
+  n=100          time: 0.0933
+  n=1,000        time: 0.7274
+  n=10,000       time: 2.2587
+  n=100,000      time: 19.358
+  n=1,000,000    time: 155.03
+  ===========================================
+
 
   ANALYSIS:
   After graphing the results, they looked most similiar to O(sqrt(n)). We don't understand our results but we can't find any errors. 
@@ -40,8 +56,36 @@ public class MergeSortTester
 We took the difference between the nanoTime of when the proccess starts and the nanoTime of when it ends. We repeated this process four times and then divided by four in order to find the average amount of time that it would take. 
      ******************************/
 
+
+    public static void populate(int[] arr, int length) {
+	for (int i = 0; i < length; i++) {
+	    int arrVal = (int)(Math.random() * 100);
+	    arr[i] = arrVal;
+	}
+    }
+
+    public static double sortTester(int length) {
+	int[] arr = new int[length];
+	populate(arr, length);
+
+	long startTime = System.nanoTime();
+	MergeSort.sort(arr);
+	long endTime = System.nanoTime();
+
+	long duration = endTime - startTime;
+	return duration/1000000f;
+    }
+
+    public static double testSort_avg(int length) {
+	double sum = 0;
+	for (int i = 0; i < 50; i++)
+	    sum += sortTester(length);
+	return sum/50;
+    }
+    
     public static void main( String[] args ) 
     {
+	/* PREVIOUS TESTING
 	int[] one = new int[1];
 	double time1 = 0.0;
        
@@ -203,8 +247,6 @@ We took the difference between the nanoTime of when the proccess starts and the 
 
 	System.out.println("Time for thousand: " + time1000/4.0);MergeSort.mess(thou);
 	
-
-
 	
 	int[] tthou = new int[10000]; 
 	double time10000 = 0.0;
@@ -231,10 +273,7 @@ We took the difference between the nanoTime of when the proccess starts and the 
 	time10000 += (end-begin)/1000.0;
 
 	System.out.println("Time for ten-thousand: " + time10000/4.0);MergeSort.mess(tthou);
-	
-
-
-	
+		
 
 	int[] hthou = new int[100000]; 
 	double time100000 = 0.0;
@@ -261,8 +300,6 @@ We took the difference between the nanoTime of when the proccess starts and the 
 	time100000 += (end-begin)/1000.0;
 
 	System.out.println("Time for hundred-thousand: " + time100000/4.0);
-
-
 	
 
 	int[] mil = new int[1000000]; 
@@ -291,9 +328,7 @@ We took the difference between the nanoTime of when the proccess starts and the 
 
 	System.out.println("Time for million: " + time1000000/4.0);
 
-
-	
-
+        
 	int[] mil0 = new int[10000000]; 
        	double time10000000 = 0.0;
 	MergeSort.mess(mil0);
@@ -320,11 +355,7 @@ We took the difference between the nanoTime of when the proccess starts and the 
 
 	System.out.println("Time for ten-million: " + time10000000/4.0);
 
-
-
-
-
-        
+  
 	int[] mil00 = new int[100000000]; 
         double time100000000 = 0.0;
 	MergeSort.mess(mil00);
@@ -350,8 +381,27 @@ We took the difference between the nanoTime of when the proccess starts and the 
 	time100000000 += (end-begin)/1000.0;
 
 	System.out.println("Time for hundred-million: " + time100000000/4.0);
+	*/
 
+	//NEW TESTING
 
+	System.out.println("TEST RUN: The time for " + "1" + " is " + sortTester(1));
+	System.out.println("The time for " + "1" + " is " + sortTester(1));
+	System.out.println("The time for " + "10" + " is " + sortTester(10));
+	System.out.println("The time for " + "100" + " is " + sortTester(100));
+	System.out.println("The time for " + "1000" + " is " + sortTester(1000));
+	System.out.println("The time for " + "10000" + " is " + sortTester(10000));
+	System.out.println("The time for " + "100000" + " is " + sortTester(100000));
+	System.out.println("The time for " + "1000000" + " is " + sortTester(1000000));
+		  
+	//TESTING FROM 500 TO 10000 IN INTERVALS OF 500
+	System.out.println("TESTING FROM 500 - 20000");
+	for (int x = 500; x <= 20000; x += 500) {
+	    System.out.println("The time for " + x + " is " + sortTester(x));
+	}
+     
+
+	
     }//end main
 
 }//end class
